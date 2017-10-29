2e0bd2b9fcc2c30abdfe94e57e82b26e7ef17c86
Anne <anne.derenoncourt@gmail.com>
2017-10-29 13:18:51 -0300

    architecture appbundle et configuration security

---
+++ src/AppBundle/Controller/MonCompteController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MonCompteController extends Controller
+{
+    /**
+     * @Route("/admin")
+     */
+    public function adminAction()
+    {
+        return new Response('<html><body>Admin page!</body></html>');
+    }
+}
