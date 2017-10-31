dd00557881d778791b8ebd61ab5ced96f336b70e
Anne <anne.derenoncourt@gmail.com>
2017-10-30 23:50:54 -0300

    formulaire inscription

---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('email',EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('status',ChoiceType::class, array(
+                'choices'=> array('particulier'=>1, 'naturaliste'=>2 )
+            ))
+            ->add('birthdate',DateType::class)
+            ->add('cgu', CheckboxType::class, array('required' => true))
+            ->add('save',SubmitType::class );
+    }
+    
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_user';
+    }
+
+
+}
