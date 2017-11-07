c25768124a96381450817f3e88209b397b93ea08
Anne <anne.derenoncourt@gmail.com>
2017-11-07 01:22:45 -0300

    autocompletion

---
+++ web/js/aves.js
@@ -0,0 +1,20 @@
+
+$( "#autocomplete" ).autocomplete({
+    source:  [
+    "Accenteur alpin",
+    "Accenteur mouchet",
+    "Agami trompette",
+    "Aigle",
+    "Aigle botté",
+    "Aigle de Verreaux",
+    "Aigle noir",
+    "Aigle des steppes",
+    "Aigle ravisseur",
+    "Aigrette",
+    "Albatros",
+    "Alouette",
+    "Amadine",
+    "Amazone",
+    "Amazone dmazone à front bleu"]
+});
+
